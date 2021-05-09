ca9d2aa091a3371fea307cc31eebd6585c8861d9
Jchu4 <jerome.chuame@gmail.com>
2021-05-09 11:42:49 +0800

    Add init_tables file

--- migrations/20210509025051-create-table.js
+++ migrations/20210509025051-create-table.js
@@ -1,11 +1,70 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('cars', {
-
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      model: {
+        type: Sequelize.TEXT,
+      },
+      photo: {
+        type: Sequelize.TEXT,
+      },
+      seats: {
+        type: Sequelize.INTEGER,
+      },
+      rating: {
+        type: Sequelize.INTEGER,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
     });
 
     await queryInterface.createTable('bookings', {
-
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      car_id: {
+        references: {
+          model: 'cars',
+          key: 'id',
+        },
+        type: Sequelize.INTEGER,
+      },
+      pickup_location: {
+        type: Sequelize.TEXT,
+      },
+      dropoff_location: {
+        type: Sequelize.TEXT,
+      },
+      pickup_date: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      return_date: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
     });
   },
 
